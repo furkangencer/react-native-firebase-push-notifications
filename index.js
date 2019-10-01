f3310c94c24308e7be1fcf3a4f66c0ce1487a076
Furkan <furkan.gencer@setrow.com>
2019-10-01 17:18:09 +0300

    Remove bgMessaging and export in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import bgMessaging from './SetrowPush/bgMessaging';
+import {backgroundMessaging} from './wdc-push';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => backgroundMessaging);
